8c496e7057065199df5fc944c871f1c4b7e6424d
Robertaaaaa <1216332407@qq.com>
2019-01-22 21:23:47 +0800

    1-完成了项目初始化

--- src/app.js
+++ src/app.js
@@ -1,22 +1,19 @@
-//导包
+// 导包
 const express = require('express')
-const path = require('path')
 
-//创建app
+// 创建app
 const app = express()
 
-//设置静态资源根目录
-app.use(express.static(path.join(__dirname,"public")))
-
-//导入路由对象
-const accountRouter = require(path.join(__dirname,"routers/accountRouter.js"))
-app.use('/account',accountRouter)
-
-//启动
-app.listen(3000,'127.0.0.1',err=>{
-    if(err){
-        console.log(err)
-    }
-
-    console.log("start ok")
+// 处理请求
+app.get('/',(req,rs)=>{
+  res.send('Hello World')
 })
+
+// 启动
+app.listen(3000,'127.0.0.1',err=>{
+  if(err){
+    console.log(err)
+  }
+  console.log("start ok")
+})
+
